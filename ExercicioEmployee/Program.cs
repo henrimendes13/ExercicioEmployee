92407dab3e2cd367e053285670a2cce7883ac8ee
Henrique Mendes <henriquemendes13@gmail.com>
2024-01-31 09:44:55 -0300

    Add project files.

---
+++ ExercicioEmployee/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExercicioEmployee.Entities;
+using System.Text.Json.Serialization;
+
+Console.Write("Enter full file path: ");
+string path = Console.ReadLine();
+
+List<Employee> list = new List<Employee>();
+
+using (StreamReader sr = File.OpenText(path))
+{
+    while (!sr.EndOfStream)
+    {
+        string[] line = sr.ReadLine().Split(',');
+        string name = line[0];
+        string email = line[1];
+        double salary = double.Parse(line[2]);
+
+        list.Add(new Employee(name, email, salary));
+    }
+
+    Console.Write("Enter salary: ");
+    double salary2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Email of people whose salary is more than {salary2}: ");
+
+    var emails = list.
+        Where(x => x.Salary > salary2).
+        OrderBy(x => x.Email).
+        Select(x => x.Email);
+    
+    foreach (string email  in emails)
+    {
+        Console.WriteLine(email);
+    }
+
+}
+
+
